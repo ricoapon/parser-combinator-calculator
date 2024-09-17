31e02052c7feb83782c851b088ff3df32e4f7b8e
Rico Apon <Rico.Apon@keylane.com>
2024-09-17 15:02:57 +0200

    Add tests for calculator that need to succeed

---
+++ app/src/test/kotlin/nl/ricoapon/calculator/combinators/CalculatorTest.kt
@@ -0,0 +1,55 @@
+package nl.ricoapon.calculator.combinators
+
+import nl.ricoapon.calculator.types.ParseResult
+import nl.ricoapon.calculator.types.ParseResult.Success
+import org.junit.jupiter.api.Nested
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class CalculatorTest {
+    private val calculator = expression()
+
+    private fun ParseResult<Double>.hasResult(result: Int) {
+        hasResult(result.toDouble())
+    }
+
+    private fun ParseResult<Double>.hasResult(result: Double) {
+        assertTrue(this is Success)
+        assertEquals(result, this.data)
+        assertTrue(this.rest.isEmpty(), "Leftover text should be empty")
+    }
+
+    @Nested
+    inner class SingleOperator {
+        @Test
+        fun plusWorks() {
+            calculator.parse("4+56").hasResult(60)
+            calculator.parse("4+5+2").hasResult(11)
+        }
+
+        @Test
+        fun minusWorks() {
+            calculator.parse("10-2").hasResult(8)
+            calculator.parse("10-1-2").hasResult(7)
+        }
+
+        @Test
+        fun timesWorks() {
+            calculator.parse("10*2").hasResult(20)
+            calculator.parse("10*3*4").hasResult(120)
+        }
+
+        @Test
+        fun divideWorks() {
+            calculator.parse("1/0").hasResult(Double.POSITIVE_INFINITY)
+            calculator.parse("6/2/3").hasResult(1)
+        }
+
+        @Test
+        fun powerWorks() {
+            calculator.parse("2^3").hasResult(8)
+            calculator.parse("2^3^2").hasResult(512)
+        }
+    }
+}
